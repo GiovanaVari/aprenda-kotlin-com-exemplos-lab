ee4ed250ca3b8ec6a3ae25729788b210be90a5b0
GiovanaVari <71236491+GiovanaVari@users.noreply.github.com>
2023-10-02 14:13:50 -0300

    Cadastro e exibicao de inscritos e cursos

--- desafio.kt
+++ desafio.kt
@@ -1,21 +1,67 @@
 // [Template no Kotlin Playground](https://pl.kotl.in/WcteahpyN)
 
-enum class Nivel { BASICO, INTERMEDIARIO, DIFICIL }
+enum class Nivel { BASICO, INTERMEDIARIO, AVANCADO }
 
-class Usuario
+data class Usuario(val nome: String, val email: String)
 
-data class ConteudoEducacional(var nome: String, val duracao: Int = 60)
+data class ConteudoEducacional(val nome: String, val duracao: Int = 60)
 
-data class Formacao(val nome: String, var conteudos: List<ConteudoEducacional>) {
+data class Formacao(val nome: String, val conteudos: List<ConteudoEducacional>, val nivel: Nivel) {
 
     val inscritos = mutableListOf<Usuario>()
     
     fun matricular(usuario: Usuario) {
-        TODO("Utilize o parâmetro $usuario para simular uma matrícula (usar a lista de $inscritos).")
+         inscritos.add(usuario)
+    }
+    
+    fun exibirListaInscritos(){
+        for(user in inscritos){
+            println(user.nome)
+        }
+    }
+    
+    fun exibirGradeFormacao(){
+        for(cont in conteudos){
+            println("${cont.nome} - ${cont.duracao}")
+        }
     }
 }
 
 fun main() {
-    TODO("Analise as classes modeladas para este domínio de aplicação e pense em formas de evoluí-las.")
-    TODO("Simule alguns cenários de teste. Para isso, crie alguns objetos usando as classes em questão.")
+    
+    //Criando variaveis dos cursos individuais
+    val moduloCSharp = ConteudoEducacional("curso c#", 90)
+    val moduloJava = ConteudoEducacional("curso java", 110)
+    val moduloKotlin = ConteudoEducacional("curso kotlin", 100)
+    val moduloLogica = ConteudoEducacional("curso logica", 70)
+    
+    val listConteudos = mutableListOf<ConteudoEducacional>()
+    listConteudos.add(moduloCSharp)
+    listConteudos.add(moduloJava)
+    listConteudos.add(moduloKotlin)
+    listConteudos.add(moduloLogica)
+    
+    //Criando variavel da formação completa com os cursos
+    val formacaoProgramacao = Formacao("Formação Programação", listConteudos, Nivel.INTERMEDIARIO)
+    
+    //Criando a lista de usuarios
+    val rose = Usuario("rose", "rose@gmail.com")
+    val pedro = Usuario("pedro", "pedro@gmail.com")
+    val juliano = Usuario("juliano", "juliano@gmail.com")
+    val rafaela = Usuario("rafaela", "rafaela@gmail.com")
+    
+    //Matriculando usuarios na formação
+    formacaoProgramacao.matricular(rose)
+    formacaoProgramacao.matricular(pedro)
+    formacaoProgramacao.matricular(juliano)
+    formacaoProgramacao.matricular(rafaela)
+    
+    println("---------------------------------------")
+    println("Dados da formação: ")
+    println("${formacaoProgramacao.nome} - ${formacaoProgramacao.nivel}")
+    println("\nGrade formação: ")
+    println(formacaoProgramacao.exibirGradeFormacao())
+    println("\n---------------------------------------")
+    println("Lista de inscritos: ")
+    println(formacaoProgramacao.exibirListaInscritos())
 }
